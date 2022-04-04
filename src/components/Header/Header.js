8bce0129b5624f98389e725cb42b2ec9b116a1f3
pushpitanandy <nandypushpita@gmail.com>
2022-04-04 23:05:59 +0600

    custom links added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import CustomLink from '../CustomLink/CustomLink';
+
 
 const Header = () => {
     return (
@@ -9,8 +11,12 @@ const Header = () => {
             <div className='product-name'>
                 <h3><FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon> Nespresso</h3>
             </div>
-            <nav>
-
+            <nav className='custom-links'>
+                <CustomLink to='/home'>Home</CustomLink>
+                <CustomLink to='/reviews'>Reviews</CustomLink>
+                <CustomLink to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink to='/blogs'>Blogs</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
             </nav>
         </div>
     );
