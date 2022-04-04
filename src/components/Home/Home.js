1a875976d8cca7f8eb0455a88274f856b5d148c2
pushpitanandy <nandypushpita@gmail.com>
2022-04-05 00:01:40 +0600

    home component edited

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is Home</h1>
+        <div className='home'>
+            <div className="home-content">
+                <h1>Best Coffee Machine</h1>
+                <p>The quality of a morning coffee can make or break your day. So for coffee lovers, it's crucial that you find a coffee maker or espresso machine that makes coffee just the way you like it. A reliable model feels like an investment, a terrible one like daylight robbery.</p>
+            </div>
+            <div className="product-picture">
+                <img src="https://dam.delonghi.com/902x902/assets/113586" alt="" />
+            </div>
         </div>
     );
 };
