635be740768231b375c2908050a73ea9f9257d4a
pushpitanandy <nandypushpita@gmail.com>
2022-04-05 19:18:35 +0600

    Not Found page updated

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
         <div>
-            <h1>page not found</h1>
+            <img src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?t=st=1649164181~exp=1649164781~hmac=94eda1181e240676482d6e5fca0caaf98843ac7fd585a072df1f525b617fccc4&w=740" alt="" />
+            <h1>Page Not Found</h1>
         </div>
     );
 };
