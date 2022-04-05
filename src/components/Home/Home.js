8fff994cb8b85ca1848b48b8922b6b8773ea8db9
pushpitanandy <nandypushpita@gmail.com>
2022-04-05 19:04:21 +0600

    home updated with limited reviews and show all btn

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,15 +13,23 @@ const Home = () => {
                 <div className="home-content">
                     <h1>Best Coffee Machine</h1>
                     <p>The quality of a morning coffee can make or break your day. So for coffee lovers, it's crucial that you find a coffee maker or espresso machine that makes coffee just the way you like it. A reliable model feels like an investment, a terrible one like daylight robbery.</p>
-                    <button onClick={() => navigate('/about')} className='find-out-more-btn'>Find Out More!</button>
+                    <button onClick={() => navigate('/about')} className='show-more-btn'>Find Out More!</button>
                 </div>
                 <div className="product-picture">
                     <img src="https://dam.delonghi.com/902x902/assets/113586" alt="" />
                 </div>
             </div>
             <div className='review-container'>
-                <h1>Customer Reviews: </h1>
-                <ReviewCard></ReviewCard>
+                <h1>Customer Reviews: {reviews.length} </h1>
+                <div className='review-display'>
+                    {
+                        reviews.map(review => <ReviewCard
+                            key={review.id}
+                            review={review}
+                        ></ReviewCard>).slice(0, 3)
+                    }
+                </div>
+                <button onClick={() => navigate('/reviews')} className='show-more-btn'>Show All Reviews</button>
             </div>
         </div>
     );
