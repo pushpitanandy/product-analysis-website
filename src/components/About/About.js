62e637bc996bd00a2a3bd47ab46d57038c759766
pushpitanandy <nandypushpita@gmail.com>
2022-04-05 15:41:45 +0600

    About component updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about us</h1>
+        <div className='about'>
+            <h1>Nespresso Coffee Machine</h1>
+            <div className='product-info'>
+                <div className="product-picture">
+                    <img src="https://dam.delonghi.com/902x902/assets/113586" alt="" />
+                </div>
+                <div className="product-detail">
+                    <h2>Features:</h2>
+                    <ul>
+                        <li>25-second heat-up time using a Thermoblock system</li>
+                        <li>Integrated manual milk frother with an included whisk</li>
+                        <li>Pre-set functions to make various types of coffee</li>
+                    </ul>
+                    <h3>Price: $449</h3>
+                </div>
+            </div>
         </div>
     );
 };
